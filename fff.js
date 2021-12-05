51d67bbed71b46693e103945f12f3e1c4ae57621
Markuvines <shinigami_master@mail.ru>
2021-12-05 11:30:15 +0400

    initial commit

---
+++ fff.js
@@ -0,0 +1,29 @@
+console.log('ffffd');
+function modalWindowToggle(){
+  $('.menuMobile').toggleClass('active');
+  $('.modalLayer').toggle();
+  $('body').toggleClass('body_modal')
+}
+$("li").on('click', function() {
+  $(this).siblings('.menuSection_active').removeClass('menuSection_active');
+  $(this).addClass("menuSection_active");
+  // console.log('ffffd');
+});
+if ($(window).width()<=375) {
+  $('.owner').prop('src', './img/ownerMin.png');
+  $('.aboutMe br').remove()
+}
+$('.menuButton').on('click', function() {
+  // $('.menuMobile').toggleClass('active');
+  // $('.modalLayer').toggle();
+  // $('body').toggleClass('body_modal')
+  modalWindowToggle();
+})
+$('.closeButton').on('click', function() {
+  // $('.menuMobile').toggleClass('active');
+  // $('.modalLayer').toggle();
+  // $('body').toggleClass('body_modal')
+  modalWindowToggle();
+})
+$('.menuMobile__menu').children().on('click', modalWindowToggle)
+// console.log($(window).width());
